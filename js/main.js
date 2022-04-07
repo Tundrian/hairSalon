2acd5058509fbeea20aa2969819d35f580741d6c
Tundrian <antoniosansotta@gmail.com>
2022-04-07 12:01:01 -0400

    Updated test JS code

--- js/main.js
+++ js/main.js
@@ -7,6 +7,6 @@ document.querySelector(".nav-icon").addEventListener("click", handleMobileMenu)
 // Functions
 function handleMobileMenu(e) {
     const mobileMenu = window.getComputedStyle(document.querySelector('.nav-mobile-menu')).display
-    console.log(mobileMenu)
+    // console.log(mobileMenu)
     mobileMenu === "flex" ? document.querySelector('.nav-mobile-menu').style.display = "none" : document.querySelector('.nav-mobile-menu').style.display = "flex"
 }
